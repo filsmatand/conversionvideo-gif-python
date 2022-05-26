d3d55211a9784a4133e280e233a188dbda0d282f
scof-code <84810234+filsmatand@users.noreply.github.com>
2022-05-27 00:41:41 +0200

    Add files via upload

---
+++ inversion_image.py
@@ -0,0 +1,11 @@
+
+from PIL import Image, ImageSequence
+import sys, os
+filename = sys.argv[1]
+im = Image.open(filename)
+original_duration = im.info['duration']
+frames = [frame.copy() for frame in ImageSequence.Iterator(im)]    
+frames.reverse()
+
+from images2gif import writeGif
+writeGif("reverse_" + os.path.basename(filename), frames, duration=original_duration/1000.0, dither=0)
